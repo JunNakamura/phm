4c42fab2dc7d674eb2a8e7d868b267a6735798bf
中村順 <n.slenderness@gmail.com>
2015-10-25 14:25:39 +0900

    #1 UserテーブルのSQLを追加

---
+++ db/migration/V1__add_user_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `phm`.`User` (
+  `id` BIGINT NOT NULL,
+  `employee_number` VARCHAR(45) NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `sex` VARCHAR(10) NOT NULL,
+  `birthday` DATE NOT NULL,
+  `hire_date` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `employee_number_UNIQUE` (`employee_number` ASC))
+ENGINE = InnoDB;
