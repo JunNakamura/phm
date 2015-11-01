126b2e02314da4f16a614b32ae62cf83e661e218
中村順 <n.slenderness@gmail.com>
2015-11-01 19:04:59 +0900

    #8 selectのhelperメソッドに渡すoptionsのリストのstaticメソッドを追加

--- app/models/constants/Sex.java
+++ app/models/constants/Sex.java
@@ -2,6 +2,11 @@ package models.constants;
 
 import com.avaje.ebean.annotation.EnumValue;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 性別の定数クラス.
  * Created by nakamurajun on 2015/10/25.
@@ -18,5 +23,15 @@ public enum Sex {
      * 女性.
      */
     @EnumValue("female")
-    FEMALE
+    FEMALE;
+
+    /**
+     * すべての要素.
+     * @return
+     */
+    public static List<String> valuesAsList() {
+        return Arrays.asList(values()).stream()
+                .map(x -> x.name())
+                .collect(Collectors.toList());
+    }
 }
