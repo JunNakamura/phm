2d412cf24adcb4182c20f15d740a8061728384c0
中村順 <n.slenderness@gmail.com>
2015-11-02 23:54:35 +0900

    #8 職歴のモデルクラス追加

---
+++ app/models/WorkExperience.java
@@ -0,0 +1,100 @@
+package models;
+
+import com.avaje.ebean.Model;
+import com.avaje.ebean.annotation.CreatedTimestamp;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+/**
+ * 職歴のモデルクラス.
+ * Created by nakamurajun on 2015/11/02.
+ */
+@Entity
+public class WorkExperience extends Model {
+
+    /**
+     * DB上のID.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    /**
+     * 紐づくユーザ.
+     */
+    @ManyToOne(optional = false)
+    public User user;
+
+    /**
+     * 業務の概要.
+     */
+    public String overview;
+
+    /**
+     * 開始日.
+     */
+    public LocalDate startDate;
+
+    /**
+     * 終了日.
+     */
+    public LocalDate endDate;
+
+    /**
+     * プログラミング言語.
+     */
+    public String programingLanguage;
+
+    /**
+     * RDMS.
+     */
+    public String rdbms;
+
+    /**
+     * フレームワーク.
+     */
+    public String framework;
+
+    /**
+     * IDE.
+     */
+    public String ide;
+
+    /**
+     * OS.
+     */
+    public String os;
+
+    /**
+     * VCS.
+     */
+    public String vcs;
+
+    /**
+     * BTS.
+     */
+    public String bts;
+
+    /**
+     * 作成日.
+     */
+    @CreatedTimestamp
+    public LocalDateTime createdAt;
+
+    /**
+     * 更新日.
+     */
+    @Version
+    public LocalDateTime updatedAt;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+}
