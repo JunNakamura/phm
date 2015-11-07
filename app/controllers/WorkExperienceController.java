4783c412539e5a6f46eb6030c76cd7120b440654
中村順 <n.slenderness@gmail.com>
2015-11-07 22:07:15 +0900

    #12 職歴コントローラの追加、追加画面のルーティングを追加

---
+++ app/controllers/WorkExperienceController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * 職歴のコントローラクラス.
+ * Created by nakamurajun on 2015/11/07.
+ */
+public class WorkExperienceController extends Controller {
+
+    /**
+     * 職歴の追加画面.
+     * @param userId
+     * @return
+     */
+    public Result displayInput(Long userId) {
+        return ok("TODO");
+    }
+}
